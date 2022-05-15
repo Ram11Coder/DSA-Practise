e9c6ec36654403c0bcaf0b2722db889d55d3eb46
Ram11Coder <rambeliver11@gmail.com>
2022-05-16 00:03:21 +0530

    Updated condition check

--- DataStruture/sorting/BubbleSort.java
+++ DataStruture/sorting/BubbleSort.java
@@ -6,20 +6,24 @@ public class BubbleSort {
 	public static void main(String[] args) {
 
 		int[] arr = { 5, 2, 6, 9, 8, 7 };
-
 		sort(arr, arr.length);
 
 	}
 
 	private static void sort(int[] arr, int n) {
 		for (int i = 0; i < n; i++) {
+			boolean isSwap = false;
 			for (int j = 0; j < n - i - 1; j++) {
 				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
+					isSwap = true;
 				}
 			}
+			if (!isSwap)
+				break;
+
 		}
 		System.out.println(Arrays.toString(arr));
 	}
