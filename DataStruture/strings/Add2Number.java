7c32c71e8c81e59a1e6324fa586729df6f2fd48d
Ram11Coder <rambeliver11@gmail.com>
2022-05-27 00:05:11 +0530

    Updated testcases

--- DataStruture/strings/Add2Number.java
+++ DataStruture/strings/Add2Number.java
@@ -2,18 +2,27 @@ package strings;
 
 public class Add2Number {
 	public static void main(String[] args) {
+		add2Numbers("9999456", "756");
+		add2Numbers("1", "1");
+		add2Numbers("9", "9");
+		add2Numbers("1111111", "11111111");
 
-		String s1 = "9999456", s2 = "756";
+		add2Numbers("999999", "999999");
+		add2Numbers("900009", "99990");
+		add2Numbers("0987654321", "1234567890");
 
+	}
+
+	private static void add2Numbers(String s1, String s2) {
 		int n1 = s1.length() - 1, n2 = s2.length() - 1;
-		int carry = 0;
+		int carry = 0, sum = 0;
 		String res = "";
 
 		/**
-		 * It will handle number greater than 9
+		 * It will handle sum of number greater than 9
 		 */
 		while (n1 >= 0 && n2 >= 0) {
-			int sum = carry + s1.charAt(n1) - '0' + s2.charAt(n2) - '0';
+			sum = carry + s1.charAt(n1) - '0' + s2.charAt(n2) - '0';
 			res = sum % 10 + res;
 			carry = sum / 10;
 			n1--;
@@ -21,24 +30,20 @@ public class Add2Number {
 		}
 
 		while (n1 >= 0) {
-			int sum = carry + s1.charAt(n1) - '0';
-
+			sum = carry + s1.charAt(n1) - '0';
 			res = sum % 10 + res;
 			carry = sum / 10;
-
 			n1--;
 		}
 
 		while (n2 >= 0) {
-			int sum = carry + s2.charAt(n2) - '0';
-
+			sum = carry + s2.charAt(n2) - '0';
 			res = sum % 10 + res;
 			carry = sum / 10;
-
 			n2--;
 		}
-
-		res = carry + res;
+		if (carry != 0)
+			res = carry + res;
 		System.out.println(res);
 	}
 }
