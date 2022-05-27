89bdfcfd906cd00f4b804f94974156ae1979c7d0
Ram11Coder <rambeliver11@gmail.com>
2022-05-28 00:20:16 +0530

    Updated HRDSEQ

--- DataStruture/arrays/HRDSEQ.java
+++ DataStruture/arrays/HRDSEQ.java
@@ -1,22 +1,31 @@
 package arrays;
 
+import java.util.Arrays;
+//https://www.codechef.com/problems/HRDSEQ
 public class HRDSEQ {
 	public static void main(String[] args) {
-		int n = 7;
+		int n = 127;
 		solution(n);
 	}
 
-	private static void solution(int n) {
-		int freq[]=new int[129];//as per constraints
-		int arr[]=new int[n+1];
-		for(int i=2;i<=n;i++) {
-			if(freq[arr[i-1]]==0) {
-				freq[arr[i-1]]=i-1;
-			}else {
-			arr[i]=	i-1 -freq[arr[i-1]]=i-1;
-			
-			}
-			freq[arr[i]]=i;
+	private static void solution(int val) {
+		int visited[] = new int[256];// as per constraints
+		int arr[] = new int[129];
+		for (int i = 2; i < 129; i++) {
+			if (visited[arr[i - 1]] != 0)
+				arr[i] = i - 1 - visited[arr[i - 1]];
+			visited[arr[i - 1]] = i - 1;
 		}
+		/*
+		 * System.out.println(Arrays.toString(arr));
+		 * System.out.println(Arrays.toString(visited));
+		 */
+		int count = 0;
+
+		for (int i = 1; i <= val; i++) {
+			if (arr[val] == arr[i])
+				count++;
+		}
+		System.out.println(count);
 	}
 }
