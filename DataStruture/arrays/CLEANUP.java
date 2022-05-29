1f9858b8475a425140f4d58caebf233e4c921781
Ram11Coder <rambeliver11@gmail.com>
2022-05-30 00:09:42 +0530

    Updated CLEANUP

--- DataStruture/arrays/CLEANUP.java
+++ DataStruture/arrays/CLEANUP.java
@@ -1,32 +1,33 @@
 package arrays;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 //https://www.codechef.com/problems/CLEANUP
 public class CLEANUP {
 	public static void main(String[] args) {
-		int n = 3;
-		int jobs[] = new int[n + 1];
-		int finsihedJobs[] = { 3,2 };
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int jobs[] = new int[n + 1];
+			int m = sc.nextInt();
+			int finsihedJobs[] = new int[m];
+			for (int i = 0; i < finsihedJobs.length; i++)
+				jobs[sc.nextInt()] = 1;
 
-		for (int i : finsihedJobs) {
-			jobs[i] = 1;
-		}
-		int unfinishedJobs[] = new int[n - finsihedJobs.length];
-		for (int i = 1, j = 0; i < jobs.length; i++) {
-			if (jobs[i] == 0)
-				unfinishedJobs[j++] = i;
-		}
-		System.out.println(Arrays.toString(unfinishedJobs));
+			int unfinishedJobs[] = new int[n - finsihedJobs.length];
+			for (int i = 1, j = 0; i < jobs.length; i++)
+				if (jobs[i] == 0)
+					unfinishedJobs[j++] = i;
 
-		for (int i = 0; i < unfinishedJobs.length; i += 2) {
-			System.out.print(unfinishedJobs[i] + " ");
-		}
-		System.out.println();
-		for (int i = 1; i < unfinishedJobs.length; i += 2) {
-			System.out.print(unfinishedJobs[i] + " ");
-		}
-		if (unfinishedJobs.length == 1)
+			for (int i = 0; i < unfinishedJobs.length; i += 2)
+				System.out.print(unfinishedJobs[i] + " ");
 			System.out.println();
+
+			for (int i = 1; i < unfinishedJobs.length; i += 2)
+				System.out.print(unfinishedJobs[i] + " ");
+			System.out.println();
+		}
+		sc.close();
 	}
 }
