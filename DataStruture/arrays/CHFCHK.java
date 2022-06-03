18fa4db42e7c3355774c1b9b9a1396f624278697
Ram11Coder <rambeliver11@gmail.com>
2022-06-03 23:57:22 +0530

    Added CHFCHK

--- DataStruture/arrays/CHFCHK.java
+++ DataStruture/arrays/CHFCHK.java
@@ -1,5 +1,21 @@
 package arrays;
 
-public class CHFCHK {
+import java.util.Scanner;
 
+//https://www.codechef.com/problems/CHFCHK
+public class CHFCHK {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int arr[] = new int[100];
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			for (int i = 0; i < n; i++)
+				arr[i] = sc.nextInt();
+
+			if (arr[0] < Integer.MAX_VALUE)
+				System.out.println(arr[0]);
+		}
+		sc.close();
+	}
 }
