075d3ff3892033ed750e7e93f45dc451b3e8784e
Ram11Coder <rambeliver11@gmail.com>
2022-06-04 23:55:25 +0530

    Added CASH

--- DataStruture/arrays/CASH.java
+++ DataStruture/arrays/CASH.java
@@ -1,5 +1,23 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class CASH {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			long sum = 0;
+			for (int i = 0; i < n; i++)
+				sum += sc.nextInt();
+
+			System.out.println(sum % k);
+
+		}
+		sc.close();
+	}
 
 }
