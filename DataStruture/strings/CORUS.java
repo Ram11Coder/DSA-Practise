fe440f1dd3a9ad40ab7b628a444f842b5307d484
Ram11Coder <rambeliver11@gmail.com>
2022-06-08 23:52:44 +0530

    Added CORUS

--- DataStruture/strings/CORUS.java
+++ DataStruture/strings/CORUS.java
@@ -1,5 +1,21 @@
 package strings;
 
-public class CORUS {
+import java.util.Arrays;
 
+//https://www.codechef.com/problems/CORUS
+public class CORUS {
+	public static void main(String[] args) {
+
+		String str = "stayinghomesaveslife";
+		int freq[] = new int[26];
+		int k = 3, count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			freq[str.charAt(i) - 'a']++;
+			if (freq[str.charAt(i) - 'a'] > k)
+				count++;
+
+		}
+		// System.out.println(Arrays.toString(freq));
+		System.out.println(count);
+	}
 }
