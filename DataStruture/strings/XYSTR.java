e3d64b6b7b1c188fc5bcd3f66f8f8e535c0ab899
Ram11Coder <rambeliver11@gmail.com>
2022-06-09 22:52:47 +0530

    Added XYSTR

--- DataStruture/strings/XYSTR.java
+++ DataStruture/strings/XYSTR.java
@@ -1,5 +1,16 @@
 package strings;
 
+//https://www.codechef.com/problems/XYSTR
 public class XYSTR {
+	public static void main(String[] args) {
 
+		String pairs = "xyxxyxx";
+		int pairCount = 0;
+		// pairs.length()-1 to avoid index out of bound condition
+		for (int i = 0; i < pairs.length() - 1; i += 2)
+			if (pairs.charAt(i) == 'x' && pairs.charAt(i + 1) == 'y'
+					|| pairs.charAt(i) == 'y' && pairs.charAt(i + 1) == 'x')
+				pairCount++;
+		System.out.println(pairCount);
+	}
 }
