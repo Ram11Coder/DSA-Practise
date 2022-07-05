e2837be51a3ac49482132db8b201700245e9700b
Ram11Coder <rambeliver11@gmail.com>
2022-07-05 23:05:39 +0530

    Added PRICECON

--- DataStruture/arrays/PRICECON.java
+++ DataStruture/arrays/PRICECON.java
@@ -1,5 +1,25 @@
 package arrays;
 
-public class PRICECON {
+import java.util.Scanner;
 
+//https://www.codechef.com/problems/PRICECON
+public class PRICECON {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			int n = sc.nextInt();
+			int p = sc.nextInt();
+			int arr[] = new int[n];
+			int sum = 0;
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+				if (arr[i] > p)
+					sum += arr[i] - p;
+			}
+			System.out.println(sum);
+		}
+		sc.close();
+	}
 }
